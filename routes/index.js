7f18a15d99ec9bcb765466dd61350204b595758e
Ryan White <mrwhiterk@gmail.com>
2019-11-08 12:39:56 -0500

    got create User route working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const User = require('../models/user')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+router.post('/', async (req, res) => {
+  try {
+    const me = new User({
+      username: 'mrwhiterk',
+      email: 'mrwhiterk@gmail.com',
+      password: '1234doggy'
+    })
+
+    await me.save()
+
+    res.send(me)
+  } catch (err) {
+    res.send({ err })
+  }
+})
+
+module.exports = router
