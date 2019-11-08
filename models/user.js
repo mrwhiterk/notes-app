4d02b6d915478b7a164734a7282d0dd7070b3d4d
Ryan White <mrwhiterk@gmail.com>
2019-11-08 12:26:37 -0500

    got basic user model

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('email is invalid')
+        }
+      }
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 7,
+      validate(value) {
+        if (value.toLowerCase().includes('password')) {
+          throw new Error("password cannot include the string 'password'")
+        }
+      }
+    },
+    avatar: {
+      type: Buffer
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+
+
+module.exports = User
