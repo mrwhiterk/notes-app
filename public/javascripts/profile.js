ca9a37c841b0260b2240774c5754182f57253f5d
Ryan White <mrwhiterk@gmail.com>
2019-11-18 15:14:41 -0500

    got delete note confirm working on profile page

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,19 +1,19 @@
 
-$(function() {
-  $('.deleteNote').click(function(e) {
-    const noteId = $(this)
-      .next('#noteID')
-      .val()
+// $(function() {
+//   $('.deleteNote').click(function(e) {
+//     const noteId = $(this)
+//       .next('#noteID')
+//       .val()
 
-    if (confirm('Are you sure you want to delete this note?')) {
-      $.ajax({
-        method: 'DELETE',
-        url: `/notes/${noteId}`
-      }).done(function(msg) {
-        location.reload()
-      })
-    } else {
-      return
-    }
-  })
-})
+//     if (confirm('Are you sure you want to delete this note?')) {
+//       $.ajax({
+//         method: 'DELETE',
+//         url: `/notes/${noteId}`
+//       }).done(function(msg) {
+//         location.reload()
+//       })
+//     } else {
+//       return
+//     }
+//   })
+// })
